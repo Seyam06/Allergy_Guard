d29535f32f9e30d4396af9b2e2baec639c99dfe3
Seyam Ahmed <seyam1201@gmail.com>
2026-03-27 22:13:08 +0600

    Store

---
+++ src/main/java/com/Covix/AllergyGuard/AllergyGuardApplication.java
@@ -0,0 +1,13 @@
+package com.Covix.AllergyGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergyGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllergyGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Covix/AllergyGuard/AllergyGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Covix.AllergyGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllergyGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
